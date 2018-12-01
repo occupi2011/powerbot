21b6492e0cf2c7ac76a487630115287dd1593bb8
occupi2011 <44349404+occupi2011@users.noreply.github.com>
2018-11-30 18:03:25 -0800

    Add files via upload

--- powerbot.ps1
+++ powerbot.ps1
@@ -55,12 +55,9 @@ Function Install-Updates () {
         if ($response -match "[Yy]")
         {
             (New-Object System.Net.WebClient).DownloadFile($updatefile, $updatepath)
-            Write-Host $updatepath
-            Write-Host $MyInvocation.ScriptName
             if((Get-FileHash -Path $updatepath).Hash -eq (Get-FileHash -Path $MyInvocation.ScriptName).Hash)
             {
                 Write-Host "New script has same hash as current script. No update necessary."
-                Write-Host "Exiting..."
                 break
             }
             Rename-Item -Path $MyInvocation.ScriptName -NewName ($MyInvocation.ScriptName+".old")
